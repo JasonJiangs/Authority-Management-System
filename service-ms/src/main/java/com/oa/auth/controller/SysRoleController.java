4ea909b42d97a02339bddc9bb8ae67b325d9cce2
jason_glb <shiyujia@kean.edu>
2023-03-07 19:54:27 -0500

    integrate knife4j

--- service-ms/src/main/java/com/oa/auth/controller/SysRoleController.java
+++ service-ms/src/main/java/com/oa/auth/controller/SysRoleController.java
@@ -3,6 +3,8 @@ package com.oa.auth.controller;
 import com.oa.auth.service.SysRoleService;
 import com.oa.common.result.ResultCode;
 import com.oa.model.system.SysRole;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,7 @@ import java.util.List;
  * @Date: 2023/3/6 13:02 13 02
  */
 
+@Api(tags = "Role Management API")
 @RestController
 @RequestMapping("/admin/system/sysRole")
 public class SysRoleController {
@@ -29,6 +32,7 @@ public class SysRoleController {
 //        return roleList;
 //    }
 
+    @ApiOperation("Query all users information")  // test with http://localhost:8800/doc.html
     @GetMapping("findAll")
     public ResultCode findAll() {
         List<SysRole> roleList = sysRoleService.list();
