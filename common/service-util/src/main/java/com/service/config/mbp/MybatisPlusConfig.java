6ec117ab9d69a30308d8936aba5fcc085539bd69
jason_glb <shiyujia@kean.edu>
2023-03-09 19:42:05 -0500

    config update

--- common/service-util/src/main/java/com/service/config/mbp/MybatisPlusConfig.java
+++ common/service-util/src/main/java/com/service/config/mbp/MybatisPlusConfig.java
@@ -2,14 +2,13 @@ package com.service.config.mbp;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
-import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import org.apache.ibatis.plugin.Interceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @Author: Jason
@@ -19,9 +18,13 @@ import org.springframework.context.annotation.Configuration;
 /*
 https://baomidou.com/pages/2976a3/#spring-boot
  */
-@MapperScan(value = "com.oa.auth.mapper")
+
+//@MapperScan(value = "com.oa.auth.mapper")
+
 @Configuration
+@EnableTransactionManagement
 public class MybatisPlusConfig {
+
     /**
      * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
      */
@@ -32,32 +35,11 @@ public class MybatisPlusConfig {
         return interceptor;
     }
 
+
+
     @Bean
     public ConfigurationCustomizer configurationCustomizer() {
         return configuration -> configuration.setUseDeprecatedExecutor(false);
     }
-
-//    @Bean
-//    public Interceptor mybatisPlusInterceptor(){
-//        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
-//        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor(){
-//            @Override
-//            protected void handlerLimit(IPage<?> page) {
-//                long pageSize = page.getSize();
-//                if(pageSize == 0){
-//                    page.setSize(page.getTotal());
-//                }
-//                super.handlerLimit(page);
-//            }
-//        };
-//        paginationInnerInterceptor.setOverflow(true);
-//        mybatisPlusInterceptor.addInnerInterceptor(paginationInnerInterceptor);
-//        return mybatisPlusInterceptor;
-//    }
-
-//    @Bean
-//    public ConfigurationCustomizer configurationCustomizer() {
-//        return configuration -> configuration.setUseDeprecatedExecutor(false);
-//    }
 
 }
