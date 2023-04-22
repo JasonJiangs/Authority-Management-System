5f56c161a66ba4d4167b305fecc134e38f490715
jason_glb <shiyujia@kean.edu>
2023-04-22 16:26:29 -0400

    Create SysUserController.java

---
+++ service-ms/src/main/java/com/oa/auth/controller/SysUserController.java
@@ -0,0 +1,21 @@
+package com.oa.auth.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * user table 前端控制器
+ * </p>
+ *
+ * @author SYJ
+ * @since 2023-04-22
+ */
+@RestController
+@RequestMapping("/auth/sys-user")
+public class SysUserController {
+
+}
+
